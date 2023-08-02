ebfa5b3b9c7733a22790d25b6a7af4009df80d97
Guilherme Moitinho <guilhermemoitinho3165@gmail.com>
2023-08-02 13:47:32 -0300

    Projeto de Estacionamento

---
+++ Estacionamento/Models/Estacionamento.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estaciona
+{
+    internal class Estacionamento
+    {
+        public double ValorInical { get; set; }
+        public double ValorPorHora { get; set; }
+
+        List<string> Veiculo = new List<string>();
+
+        public Estacionamento(double valorInical, double valorPorHora)
+        {
+            ValorInical = valorInical;
+            ValorPorHora = valorPorHora;
+        }
+
+        public Estacionamento(List<string> veiculo)
+        {
+            Veiculo = veiculo;
+        }
+
+        public void CadastrarVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veiculo para estacionar:");
+            string NomeVeiculo = Console.ReadLine();
+            Veiculo.Add(NomeVeiculo);
+            Console.WriteLine();
+        }
+
+        public double CalculoDoTempo(double horas)
+        {
+            return ValorPorHora * horas + ValorInical; 
+        }
+
+        public void RemoverVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veiculo para Remover:");
+            string NomeVeiculo = Console.ReadLine();
+            Veiculo.FindAll(x => x .Equals(NomeVeiculo));
+            Veiculo.Remove(NomeVeiculo);
+            Console.WriteLine("Digite a quantidade de hoaras que o veiculo permaneceu parado: ");
+            double horas = double.Parse(Console.ReadLine());
+            var valor = CalculoDoTempo(horas);
+            Console.WriteLine($"O veiculo {NomeVeiculo} foi removido e o preco total foi de: R$ {valor}");
+            Console.WriteLine();
+        }
+
+        public void MostrarVeiculos()
+        {
+            Console.WriteLine("Os veiculos sao: ");
+            foreach (var i in Veiculo)
+            {
+                Console.WriteLine(i);
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+  
+    }
+}
---
+++ Estacionamento/Program.cs
@@ -0,0 +1,41 @@
+﻿using Estaciona;
+
+
+Console.WriteLine("Qual o valor inicial?");
+double ValorInicial = double.Parse(Console.ReadLine());
+Console.WriteLine("Qual o valor por hora?");
+double ValorPorHora = double.Parse(Console.ReadLine());
+
+Estacionamento estaciona = new Estacionamento(ValorInicial, ValorPorHora);
+
+Console.Clear();
+
+bool exibirMenu = true;
+
+while (exibirMenu)
+{
+    Console.WriteLine("Digite a Opcao -->\n1 - Cadastrar veiculo\n2 - Remover veiculo\n3 - Listar veiculos\n4 - Encerrar");
+    int NumEsco = int.Parse(Console.ReadLine());
+
+    Console.Clear();
+
+    switch (NumEsco)
+    {
+        case 1:
+            estaciona.CadastrarVeiculo();
+            break;
+        case 2:
+            estaciona.RemoverVeiculo();
+            break;
+        case 3:
+            estaciona.MostrarVeiculos();
+            break;
+        case 4:
+            Console.WriteLine("Fim do Programa!");
+            exibirMenu = false;
+            break;
+        default: 
+            Console.WriteLine("Opcao invalida");
+            break;
+    }
+}
